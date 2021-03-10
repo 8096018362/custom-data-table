2f7b100effde458d9b098cb03a35f9e95e5c9ef9
ramu vishwanatham <ramu@lifelinelab.io>
2021-03-10 19:45:11 +0800

    key search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,478 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  searchInputToValue: any = '';
+  searchInputFromValue: any = '';
+  selname: any = '';
+  selfieldType: any = '';
   title = 'table-angular';
+  searchArray: any = [];
+  selectedSearchObj: any;
+  openSearchFields(columnObj: any) {
+    this.selectedSearchObj = columnObj;
+    this.searchArray = columnObj.searchType;
+  }
+  public searchInputValue: any = '';
+  public campaignOne: any = {};
+  public campaignTwo: any = {};
+
+  initColumns = [
+    {
+      name: 'pkey',
+      defaultColumn: true,
+      show: true,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'number'
+        },
+        {
+          searchType: 'greterthan',
+          searchFieldType: 'number'
+        },
+        {
+          searchType: 'between',
+          searchFieldType: 'number'
+        }
+      ]
+    },
+    {
+      name: 'criticality',
+      defaultColumn: false,
+      show: false,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'assetUid',
+      defaultColumn: true,
+      show: true,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'warrantyExpiry',
+      defaultColumn: false,
+      show: false,
+      searchType: [
+        {
+          searchType: 'between',
+          searchFieldType: 'date'
+        }
+      ]
+    },
+    {
+      name: 'customerId',
+      defaultColumn: true,
+      show: true,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'description',
+      defaultColumn: true,
+      show: true,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'machineName',
+      defaultColumn: false,
+      show: false,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'make',
+      defaultColumn: false,
+      show: false,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'serialNumber',
+      defaultColumn: false,
+      show: false,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    },
+    {
+      name: 'modelNumber',
+      defaultColumn: false,
+      show: false,
+      searchType: [
+        {
+          searchType: 'single',
+          searchFieldType: 'input'
+        }
+      ]
+    }
+    // "installedDate", "warrantyExpiry", "isActive", "location", "powerRating", "supplier", "category", "criticality"];
+  ]
+
+  displayedColumns: any = [];
+  dataSource: any = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.initColumns.map(col => {
+      if (col.defaultColumn) {
+        this.displayedColumns.push(col.name)
+      }
+    });
+    setTimeout(() => {
+      this.dataSource.data = ELEMENT_DATA;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, 1000);
+  }
+
+  ngOnInit() {
+
+  }
+
+
+  columnOpearation(index: any) {
+    if (this.initColumns[index].show) {
+      this.initColumns[index].show = false;
+      this.displayedColumns.splice(index, 1);
+    } else {
+      this.initColumns[index].show = true;
+      this.displayedColumns.push(this.initColumns[index].name)
+    }
+
+  }
+
+  selectedSearchType(sObj: any) {
+    this.selfieldType = sObj.searchFieldType;
+    this.selname = sObj.searchType;
+  }
+
+
+  finalArray: any = [];
+  public fetchDataByFiltered() {
+    this.finalArray = [];
+    let mainKey = this.selectedSearchObj.name;
+    let searchType = this.selname;
+
+    let dataSource: any = ELEMENT_DATA;
+
+    if (searchType == 'single') {
+      dataSource.map((itm: any) => {
+        if (itm[mainKey] == this.searchInputValue) {
+          this.finalArray.push(itm)
+        }
+      });
+    } else if (searchType == 'greterthan') {
+      dataSource.map((itm: any) => {
+        if (itm[mainKey] < this.searchInputValue) {
+          this.finalArray.push(itm)
+        }
+      });
+    } else if (searchType == 'between') {
+
+      dataSource.map((itm: any) => {
+        if (itm[mainKey] > this.searchInputFromValue && itm[mainKey] < this.searchInputToValue) {
+          this.finalArray.push(itm)
+        }
+      });
+    }
+
+    setTimeout(() => {
+      this.dataSource = new MatTableDataSource<any>(this.finalArray);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }, 1000);
+    }, 2000);
+
+  }
+
+
+
 }
+
+const ELEMENT_DATA: any = [
+  {
+    "pkey": 1,
+    "assetUid": "ast128765",
+    "customerId": 12345,
+    "description": "hello123",
+    "machineName": "caterpillar",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:10",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 1,
+    "criticality": 2
+  },
+  {
+    "pkey": 2,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 3,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 4,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 5,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }
+  ,  {
+    "pkey": 6,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 7,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 8,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 9,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 10,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 11,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 12,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 13,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }, {
+    "pkey": 14,
+    "assetUid": "ast128766",
+    "customerId": 12345,
+    "description": "hello",
+    "machineName": "JCB",
+    "make": "2018",
+    "serialNumber": "546yhgt9uy2",
+    "modelNumber": "h343h8743h09j",
+    "installedDate": "2019-06-02T16:59:51",
+    "warrantyExpiry": "2021-12-25T10:30:50",
+    "isActive": true,
+    "location": "india",
+    "powerRating": "2",
+    "supplier": "TATA",
+    "category": 2,
+    "criticality": 2
+  }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { A11yModule } from '@angular/cdk/a11y';
+import {
+  MatGridListModule,
+} from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [MatInputModule, MatSelectModule, MatButtonModule, MatNativeDateModule, MatDatepickerModule,
+    MatTableModule, A11yModule, MatGridListModule,
+    CdkTableModule,
+    CdkTreeModule, FormsModule,
+    ReactiveFormsModule,
+    DragDropModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
